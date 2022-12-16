72abf77a8f7439d2458195056a4203df5161a7a4
Pratham <prathamsinghal2003@gmail.com>
2022-12-16 10:01:23 +0530

    :rocket: Configured with api

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
-const Header = () => {
+const Header = (props) => {
+    // const hanldeChange=(e)=>{
+    //     props.setTitle(e.target.value)
+    // }
   return (
-    <div>Header</div>
+    <div className="flex flex-row justify-between w-[95vw] mx-auto my-3 border-b-2 border-white">
+        <div>
+            <img className="w-50 h-20 m-3"
+            src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" alt="Netflix Logo" />
+        </div>
+        <div>
+            <input 
+            className="w-96 h-10 m-7 rounded-lg outline-0 text-[#000] p-3"
+            type="text" 
+            placeholder="Search for a movie" 
+            value={props.title} 
+            onChange={(e)=>props.setTitle(e.target.value)}/>
+        </div>
+    </div>
   )
 }
 
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useState,useEffect} from 'react'
 import Header from '../components/Header'
 import Card from '../components/Cards'
 
 const Index = () => {
+  const [title,setTitle] = useState("")
+  const getMovies=async (title)=>{
+    let response
+    // console.log(process.env.REACT_APP_API_KEY)
+    if(!title){
+      response= await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1&include_adult=false`)
+    }
+    else{
+      response= await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${title}&page=1&include_adult=false`)
+    }
+    const data= await response.json()
+    console.log(data)
+  }
+  useEffect(() => {
+    getMovies(title)
+  },[title])
   return (
     <div>
-      <Header/>
+      <Header title={title} setTitle={setTitle} change={getMovies}/>
       <div>
         <Card 
         // img={}
@@ -13,6 +29,7 @@ const Index = () => {
         // description={}
         // ratings={}
         />
+        {title}
       </div>
     </div>
   )
